998960e2c09e89104dd9f5128e1ce118c8f19040
Marcelo Pinto <mpinto70@gmail.com>
2017-12-25 19:57:36 -0500

    add boost

--- test/dir/fileTest.cpp
+++ test/dir/fileTest.cpp
@@ -2,11 +2,17 @@
 
 #include <gtest/gtest.h>
 
+#include <boost/filesystem.hpp>
+
 namespace dir {
 
 TEST(fileTest, square) {
     EXPECT_EQ(square(2), 4);
     EXPECT_EQ(square(3), 9);;
+}
+
+TEST(fileTest, filesystem) {
+    EXPECT_TRUE(boost::filesystem::is_regular_file(PROJECT_DIR "/CMakeLists.txt"));
 }
 
 }
