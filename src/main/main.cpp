91bcc36c55bb62246d19ef2d7732e39653d97e24
Marcelo Pinto <mpinto70@gmail.com>
2018-11-25 16:38:39 -0500

    create an executable

---
+++ src/main/main.cpp
@@ -0,0 +1,19 @@
+#include "dir/file.h"
+
+#include <iostream>
+
+class Implementation : dir::Interface {
+public:
+    Implementation() = default;
+    ~Implementation() override = default;
+    std::string function(int x) const override {
+        return std::to_string(x);
+    }
+};
+
+int main() {
+    const Implementation impl;
+
+    std::cout << dir::square(12) << std::endl;
+    std::cout << impl.function(12) << std::endl;
+}
